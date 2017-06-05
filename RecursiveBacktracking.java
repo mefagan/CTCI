8af05c71d96f9118b4ff740aa47a91f668877021
MaryEileen Fagan <maryeileen.fagan@gmail.com>
2017-06-05 19:28:24 -0400

    recursive permutations without backtracking done

--- RecursiveBacktracking.java
+++ RecursiveBacktracking.java
@@ -4,19 +4,21 @@ public class RecursiveBacktracking {
   
   public static int recurseSansBacktracking(String sample, String remaining, int solutions) {
     if (remaining.length()==0) {
+      System.out.println(sample);
       solutions++;
     }
     for (int i = 0; i < remaining.length(); i ++) {
       String rest = remaining.substring(0, i) + remaining.substring(i+1);
-      recurseSansBacktracking(sample, rest, solutions);
+      //System.out.println(rest);
+      recurseSansBacktracking(sample + remaining.charAt(i), rest, solutions);
     }
     return solutions;
   }
   
   public static void main(String[] args) {
     int solutions = 0;
-    String sample = "Mar";
-    recurseSansBacktracking(sample, sample, 0);
+    String sample = "MAR";
+    recurseSansBacktracking("", sample, 0);
   }
 
 }

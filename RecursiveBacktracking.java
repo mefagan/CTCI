74e77096bb45b23032e7d809b91be8fbd6819f2a
MaryEileen Fagan <maryeileen.fagan@gmail.com>
2017-06-06 21:36:04 -0400

    working permutation on list

--- RecursiveBacktracking.java
+++ RecursiveBacktracking.java
@@ -1,5 +1,8 @@
 package crackingCodingInterview;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecursiveBacktracking {
   
   public static int recurseSansBacktracking(String sample, String remaining, int solutions) {
@@ -9,11 +12,29 @@ public class RecursiveBacktracking {
     }
     for (int i = 0; i < remaining.length(); i ++) {
       String rest = remaining.substring(0, i) + remaining.substring(i+1);
-      //System.out.println(rest);
+      System.out.println(i);
       recurseSansBacktracking(sample + remaining.charAt(i), rest, solutions);
     }
     return solutions;
   }
+  
+  public static List<List<Integer>> recursePerm(List<List<Integer>> list, ArrayList<Integer> tempList, int[] nums) {
+    //base case solution
+    if (tempList.size() == nums.length) {
+        list.add(new ArrayList<Integer>(tempList));
+        tempList = new ArrayList<Integer>();
+    } else {
+        for (int i = 0; i < nums.length; i ++) {
+            if (tempList.contains(nums[i])) {
+                continue;
+            }
+            tempList.add(nums[i]);
+            recursePerm(list, tempList, nums);
+            tempList.remove((Integer)nums[i]);
+        }
+    }
+    return list;
+}
   
   public static int findAllSubsets(String curr, String remaining, int count) {
     if(remaining.length()==0) {
@@ -28,9 +49,19 @@ public class RecursiveBacktracking {
   
   public static void main(String[] args) {
     int solutions = 0;
-    String sample = "MAR";
+    String sample = "mar";
     //recurseSansBacktracking("", sample, 0);
-    findAllSubsets("", sample, 0);
+    //findAllSubsets("", sample, 0);
+    int[] nums = new int[3];
+    ArrayList<Integer> numAL = new ArrayList<Integer>();
+    for (int i = 0; i < nums.length; i++) {
+      nums[i] = i;
+    }
+    List<List<Integer>> list = new ArrayList<List<Integer>>();
+    list = recursePerm(list, new ArrayList<Integer>(), nums);
+    for (List<Integer> l : list) {
+      System.out.println(l);
+    }
   }
 
 }
